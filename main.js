81336d63a9b506513ccc2da559e67aa8df448ac6
zubietaroberto <zubieta.roberto.e@gmail.com>
2014-10-31 17:09:10 +0100

    Simplified Log.

--- main.js
+++ main.js
@@ -12,20 +12,10 @@ var x = new Collector(function (err) {
 		//Parse date
 		var date = new Date(0);
 		date.setUTCSeconds(packet.header.unix_secs)
-		packet.header.formatted_date = date.toGMTString();
+		var receptionDate = date.toGMTString();
 
-		//Parse running time
-		//Source: http://stackoverflow.com/questions/9763441/milliseconds-to-time-in-javascript
-		var s = packet.header.sys_uptime;
-		var ms = s % 1000;
-		s = (s - ms) / 1000;
-		var secs = s % 60;
-		s = (s - secs) / 60;
-		var mins = s % 60;
-		var hrs = (s - mins) / 60;
-		packet.header.formatted_uptime = hrs + 'H ' + mins + 'M ' + secs + 'S ' + ms + " ms";
-
-		console.log(packet);
+		//Show Log
+		console.log(receptionDate + ": Received packet from source (id: " + packet.header.source_id + ")" );
 	} 
 
 )
