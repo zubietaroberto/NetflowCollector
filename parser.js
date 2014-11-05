622528a31f519d407fcd07fa3136ed1ba5bfb0ee
zubietaroberto <zubieta.roberto.e@gmail.com>
2014-11-05 17:07:22 +0100

    Parser parses correctly the templates

--- parser.js
+++ parser.js
@@ -1,20 +1,42 @@
 
 var fieldDictionary = require('./dictionary.js')
+var templateArray = new Array()
 
 /*
 Templates
 */
 function parseTemplates(templateObject){
 	templateObject.templates.forEach(function(element, index, array){
+
+		//Logging
 		console.log("Template for " + element.id + ": " + element.fields.length + " fields")
 
-		element.fields.forEach(function(field, index2, array2){
-			var fieldType = fieldDictionary[field.fieldLength]
+		//Build the template object
+		var templateFields = new Array()
 
-			if (fieldType != 'null'){
-				console.log(fieldType)
+		// Iterate through the fields in the original packet and transform them into fieldType Objects
+		element.fields.forEach(function(field, index2, array2){
+
+			//Get a reference to the dictionary's object
+			var dictionaryElement = fieldDictionary[field.fieldType]
+
+			// null protection, in case of propietary fields we can't parse
+			if (typeof dictionaryElement !== 'undefined'){
+
+				//Add to the array a new object, a copy of the dictionary's one.
+				templateFields[index2] = {
+					length: field.fieldLength,
+					type: field.fieldType,
+					name: dictionaryElement.name
+				}
 			}
 		})
+
+		//Save to templateArray
+		templateArray[element.id] = templateFields
+
+		// Log Results
+		console.log(templateFields)
 	})
 }
 
@@ -30,6 +52,14 @@ Flowset Data
 */
 function parseFlowset(flowsetObject){
 	console.log("Flowset " + flowsetObject.flowset_id + ": Length " + flowsetObject.length)
+
+	// Get the correponding template, if it exists.
+	var templateFields = templateArray[flowsetObject.flowset_id]
+	if (typeof templateFields !== 'undefined'){
+		console.log(templateFields)
+	} else {
+		console.log("template not found")
+	}
 }
 
 
