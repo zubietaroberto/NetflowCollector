4c4fcfbf1f0de2b7aa856fa07ce85709649a87d7
zubietaroberto <zubieta.roberto.e@gmail.com>
2014-11-10 18:50:00 +0100

    Improved "decoder" require statement

---
+++ decoder/index.js
@@ -0,0 +1,5 @@
+/*
+When importing using require('./decoder'), this is the file that gets loaded
+first. We will respond with the first file to be loaded: dictionary.js
+*/
+module.exports = require('./dictionary.js')
