7e9ae56485d93c593e78abd3c3af3a8910e194b8
zubietaroberto <zubieta.roberto.e@gmail.com>
2014-11-17 17:10:12 +0100

    Moved all Express logic into "frontend" package.

--- bin/www.js
+++ bin/www.js
@@ -1,29 +1,4 @@
-var express = require('express')
-var app = express()
-var morgan = require('morgan');
-var path = require('path');
-
-//Frontend-related variables
-var frontendLocation = '../frontend';
-var frontendDirectory = path.resolve(__dirname, frontendLocation);
-
-//Import Routers
-var apiRouter = require('../api');
-var frontendRouter = require(frontendLocation);
-
-//Logging middleware
-app.use(morgan('dev'));
-
-//Jade Configuration
-app.set('views', path.join(frontendDirectory, 'views'));
-app.set('view engine', 'jade');
-app.use(express.static(path.join(frontendDirectory, 'public')));
-
-//Root route
-app.get('/', frontendRouter);
-
-//Router for the API
-app.use('/api', apiRouter);
+var app = require('../frontend');
 
 //Start server
 var server = app.listen(80, function(){
--- frontend/index.js
+++ frontend/index.js
@@ -1,3 +1,27 @@
-﻿
+﻿var express = require('express')
+var app = express()
+var morgan = require('morgan');
+var path = require('path');
+
+
+    //Import Routers
+var apiRouter = require('./apiRouter');
+var frontendRouter = require('./frontendRouter');
+
+    //Logging middleware
+app.use(morgan('dev'));
+
+    //Jade Configuration
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+app.use(express.static(path.join(__dirname, 'public')));
+
+    //Root route
+app.get('/', frontendRouter);
+
+    //Router for the API
+app.use('/api', apiRouter);
+
+
 //Reroute to the router
-module.exports = require('./frontendRouter')
+module.exports = app
