d3cdc2e7c7549c60cf1edf4b59de751ea7d989cf
zubietaroberto <zubieta.roberto.e@gmail.com>
2014-11-17 17:39:49 +0100

    Added LESS parsing for the CSS

--- frontend/index.js
+++ frontend/index.js
@@ -2,6 +2,8 @@
 var app = express()
 var morgan = require('morgan');
 var path = require('path');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
 
 
     //Import Routers
@@ -11,10 +13,18 @@ var frontendRouter = require('./frontendRouter');
     //Logging middleware
 app.use(morgan('dev'));
 
+// Parser Middleware
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+
     //Jade Configuration
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 app.use(express.static(path.join(__dirname, 'public')));
+
+    //LESS configuration
+app.use(require('less-middleware')(path.join(__dirname, 'public')));
 
     //Root route
 app.get('/', frontendRouter);
