a46faed487f0abd30289c8032acd864b650c684f
zubietaroberto <zubieta.roberto.e@gmail.com>
2014-11-19 18:45:36 +0100

    Basic  Amcharts

--- frontend/public/javascript/main.js
+++ frontend/public/javascript/main.js
@@ -1,4 +1,18 @@
 ﻿
+// Function to extract the number of bytes in the data
+function getBytes(data){
+			
+	var result = "N/A"
+
+	data.forEach(function(value, index){
+		if (value.field_type == 1){
+			result = value.value;
+		}
+	})
+	
+	return result;
+}
+
 // Function to build the template of the <table> element. Each object is a different column.
 function buildColumns(){
 
@@ -16,7 +30,7 @@ function buildColumns(){
 	result.push({
 		header: "Date",
 		data: function(value){
-			return value.time;
+			return AmCharts.formatDate(new Date(value.time), "YYYY-MM-DD HH:MM:SS:QQ");
 		}
 	});
 
@@ -26,7 +40,15 @@ function buildColumns(){
 		data: function(value){
 			return value.source_address + " - " + value.source_id
 		}
-	})
+	});
+
+	//Total Bytes Column
+	result.push({
+		header: "Bytes",
+		data: function(value){
+			return getBytes(value.data)
+		}
+	});
 
 	return result;
 }
@@ -63,6 +85,8 @@ jQuery(document).ready(function ($) {
             	tableHeaderRow.append(headerTag);
             })
 
+            var dataProvider = new Array();
+
             //Insert the Data into the data cells
             $.each(data, function(index, value){
 
@@ -76,8 +100,56 @@ jQuery(document).ready(function ($) {
             		//Build a new cell and add the value to it
             		var tag = $("<td>").append(value2.data(value));
             		tableRow.append(tag);
+
             	});
-            });
+
+            	//Add to the Dataprovider
+            	dataProvider.push({
+            		date: new Date(value.time),
+            		value: getBytes(value.data)
+            	});
+
+			});
+
+			//Activate Amcharts
+			 AmCharts.makeChart("chartdiv", {
+			    "type": "serial",
+			    "theme": "none",
+			    "pathToImages": "http://www.amcharts.com/lib/3/images/",
+			    "dataProvider": dataProvider,
+		        "graphs": [{
+					"id": "g1",
+		            "bullet": "round",
+		            "bulletBorderAlpha": 1,
+		            "bulletColor": "#FFFFFF",
+		            "bulletSize": 5,
+		            "hideBulletsCount": 50,
+		            "lineThickness": 2,
+		            "title": "red line",
+		            "useLineColorForBulletBorder": true,
+		            "valueField": "value",
+        			"type": "smoothedLine"
+		        }],
+		        "chartScrollbar": {
+					"graph": "g1",
+					"scrollbarHeight": 30
+				},
+			    "chartScrollbar": {},
+			    "chartCursor": {
+			        "categoryBalloonDateFormat": "YYYY-MM-DD HH:MM:SS:QQ",
+			        "cursorAlpha": 0,
+			        "cursorPosition": "mouse"
+			    },
+			    "dataDateFormat": "YYYY-MM-DD HH:MM:SS:QQ",
+			    "categoryField": "date",
+			    "categoryAxis": {
+			    	"minPeriod": "fff",
+			    	"parseDates": true,
+			        "minorGridAlpha": 0.1,
+			        "minorGridEnabled": true
+			    }
+
+			});
         });
     
 });
