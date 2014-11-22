5128152fdca30dd6145f8ff8a96d763611ea5347
zubietaroberto <zubieta.roberto.e@gmail.com>
2014-11-22 10:18:23 +0100

    Basic implementation of "Protocols" and "Addresses" endpoints.

---
+++ frontend/public/javascript/piechart.js
@@ -0,0 +1,16 @@
+﻿jQuery(document).ready(function ($) {
+
+    var chart = AmCharts.makeChart("chartdiv", {
+        "type": "pie",
+        "theme": "none",
+        "dataProvider": data,
+        "valueField": "count",
+        "titleField": "_id",
+        "exportConfig": {
+            menuItems: [{
+                    icon: '/lib/3/images/export.png',
+                    format: 'png'
+                }]
+        }
+    });
+});
